14c86d20e92383f449cd2cf3971f2b82af90da61
githublover0101 <conversechen@126.com>
2022-10-08 23:14:05 +0800

    add

--- src/start202209/TransactionalStack.java
+++ src/start202209/TransactionalStack.java
@@ -4,53 +4,53 @@ import java.util.Stack;
 
 public class TransactionalStack {
 
-	private Stack<Integer> content;
-	private Stack<TransactionalStack> transactions;
-	
-	public TransactionalStack() {
-		content = new Stack<Integer>();
-		transactions = new Stack<TransactionalStack>();
-		transactions.push(this);
-	}
-	
-	public void push(int value) {
-		transactions.peek().content.push(value);
-	}
-	
-	public int top() {
-		Stack<Integer> topContent = transactions.peek().content;
-		return topContent.isEmpty() ? 0 : topContent.peek();
-	}
-	
-	public int pop() {
-		Stack<Integer> topContent = transactions.peek().content;
-		return topContent.isEmpty() ? 0 : topContent.pop();
-	}
-	
-	public void begin() {
-		TransactionalStack newTransaction = new TransactionalStack();
-		newTransaction.content = (Stack<Integer>)content.clone();
-		transactions.add(newTransaction);
-	}
-	
-	public boolean rollback() {
-		TransactionalStack lastTransaction = transactions.peek();
-		if(lastTransaction != this) {
-			lastTransaction.pop();
-			return true;
-		}
-		return false;
-	}
-	
-	public boolean commit() {
-		TransactionalStack lastTransaction = transactions.peek();
-		if(lastTransaction != this) {
-			lastTransaction = transactions.pop();
-			transactions.peek().content = lastTransaction.content;
-			return true;
-		}
-		return false;
-	}
+    private Stack<Integer> content;
+    private Stack<TransactionalStack> transactions;
+
+    public TransactionalStack() {
+        content = new Stack<Integer>();
+        transactions = new Stack<TransactionalStack>();
+        transactions.push(this);
+    }
+
+    public void push(int value) {
+        transactions.peek().content.push(value);
+    }
+
+    public int top() {
+        Stack<Integer> topContent = transactions.peek().content;
+        return topContent.isEmpty() ? 0 : topContent.peek();
+    }
+
+    public int pop() {
+        Stack<Integer> topContent = transactions.peek().content;
+        return topContent.isEmpty() ? 0 : topContent.pop();
+    }
+
+    public void begin() {
+        TransactionalStack newTransaction = new TransactionalStack();
+        newTransaction.content = (Stack<Integer>) content.clone();
+        transactions.add(newTransaction);
+    }
+
+    public boolean rollback() {
+        TransactionalStack lastTransaction = transactions.peek();
+        if (lastTransaction != this) {
+            transactions.pop();
+            return true;
+        }
+        return false;
+    }
+
+    public boolean commit() {
+        TransactionalStack lastTransaction = transactions.peek();
+        if (lastTransaction != this) {
+            lastTransaction = transactions.pop();
+            transactions.peek().content = lastTransaction.content;
+            return true;
+        }
+        return false;
+    }
 	
 	public void print() {
 		for(int e: content) {
